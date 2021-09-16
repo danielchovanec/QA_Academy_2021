64901ceba9a1c2d8b95decf767f73a1cbcf2bf53
PavelPrcin <Pavol.Prcin@ness.com>
2021-09-16 08:27:17 +0200

    homework JSON_placeholder

--- PavelPrcin/cypress/support/index.js
+++ PavelPrcin/cypress/support/index.js
@@ -14,7 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
+import "./comands_request";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
