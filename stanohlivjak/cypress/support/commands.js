016db55a2696e890573e0ab03f73c0f1fb50c7f0
stanohlivjak <88368171+stanohlivjak@users.noreply.github.com>
2021-09-19 22:46:57 +0200

    Cypress homework

--- stanohlivjak/cypress/support/commands.js
+++ stanohlivjak/cypress/support/commands.js
@@ -1,20 +1,32 @@
 Cypress.Commands.add('applicationLogin', (username, password) => {
-    Cypress.on('uncaught:exception', (err, runnable) => {
-        console.log(err);
-        return false;
-    })
+    // Cypress.on('uncaught:exception', (err, runnable) => {
+    //     console.log(err);
+    //     return false;
+    // })
     cy.visit("/");
-    cy.get('.LoginFormstyles__LoginButton-sc-1iebk76-2')
+    cy.get('.Header_header__inner__2zz3f > .Button_button__ZunwN')
         .click()
-    cy.get('.Form-sc-1mfmq26-0 > :nth-child(1) > .Input-sc-1vv8hqf-0')
+    cy.get('.panel-left-border > :nth-child(2) > :nth-child(1) > .cognito-asf > :nth-child(3) > #signInFormUsername')
         .type(username)
-    cy.get('.LoginFormstyles__PasswordInputArea-sc-1iebk76-7 > .Input-sc-1vv8hqf-0')
+    cy.get('.panel-left-border > :nth-child(2) > :nth-child(1) > .cognito-asf > :nth-child(5) > #signInFormPassword')
         .type(password)
-    cy.get('.LoginFormstyles__MobileLoginMiddlePanel-sc-1iebk76-12 > .Form-sc-1mfmq26-0 > .Button-sc-1fngo4c-0')
+    cy.get('.panel-left-border > :nth-child(2) > :nth-child(1) > .cognito-asf > .btn')
         .click()
 })
 
-Cypress.Commands.add('applicationLogout', () => {
-    cy.get('.mi-ic-menu').click()
-    cy.get('.link-logout').click()
-})
+// Cypress.Commands.add('metrixApplicationLogin', (username, password) => {
+//     cy.visit("/");
+//     cy.get('.Header_header__inner__2zz3f > .Button_button__ZunwN')
+//         .click()
+//     cy.get('.panel-left-border > :nth-child(2) > :nth-child(1) > .cognito-asf > :nth-child(3) > #signInFormUsername')
+//         .type(username)
+//     cy.get('.panel-left-border > :nth-child(2) > :nth-child(1) > .cognito-asf > :nth-child(5) > #signInFormPassword')
+//         .type(password)
+//     cy.get('.panel-left-border > :nth-child(2) > :nth-child(1) > .cognito-asf > .btn')
+//         .click()
+// })
+
+// Cypress.Commands.add('applicationLogout', () => {
+//     cy.get('.mi-ic-menu').click()
+//     cy.get('.link-logout').click()
+// })
--- stanohlivjak/cypress/support/index.js
+++ stanohlivjak/cypress/support/index.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
+import './commands_request'
+import './commands_matrix_backend'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
