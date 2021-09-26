fc4ba3d28079d265d61620d4801a95bdbf58da01
danielchovanec <Daniel.Chovanec@ness.com>
2021-09-26 13:42:44 +0200

    creating file for matrix test

---
+++ chovanec/cypress/integration/back_end/backlog_record_test.js
@@ -0,0 +1,9 @@
+describe('Testing add/delete record functionality of matrix app', () => {
+    before(() => {
+        cy.applicationLogin(Cypress.env('username'), Cypress.env('password'))
+    })
+
+    it('redirecting to dashboard overview and Backlog Health details page', () =>{
+        
+    })
+})
